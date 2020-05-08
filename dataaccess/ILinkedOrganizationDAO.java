cd621409ef024cb6e920ae12be9636fb8bf075b8
MARTHA <martha_15_7@outlook.com>
2020-05-08 02:43:03 -0500

    implementacion del caso de uso Register project

--- dataaccess/ILinkedOrganizationDAO.java
+++ dataaccess/ILinkedOrganizationDAO.java
@@ -2,11 +2,16 @@ package dataaccess;
 
 import java.util.List;
 import domain.LinkedOrganization;
+/**
+ *
+ * @author: Martha M. Ortiz
+ * @version: 08/05/2020
+*/
 
 public interface ILinkedOrganizationDAO {
     public List<LinkedOrganization> getAllLinkedOrganization ();
     public LinkedOrganization getLinkedOrganization (int idOrganization);
-    public int searchLinkedOrganization (String name);
-    public void updateLinkedOrganization (LinkedOrganization organization);
-    public void actualizationOrganization (LinkedOrganization organization);
+    public int searchLinkedOrganization (String name, String email);
+    public String updateLinkedOrganization (LinkedOrganization organization);
+    public String actualizationOrganization (LinkedOrganization organization);
 }
--- logic/ValidateDataPerson.java
+++ logic/ValidateDataPerson.java
@@ -41,23 +41,4 @@ public class ValidateDataPerson {
         result = mather.find();
         return result;
     }
-
-    public boolean validateWord(String word) {
-        boolean result = true;
-        int index = 0;
-        int valueASCII;
-        int sizeWord= word.length();
-        while(result && index<sizeWord){
-            valueASCII = (int)word.charAt(index);
-            if(valueASCII == 32 && ((int)word.charAt(index - 1)) == 32){
-                result = false;
-            }else{
-                if((valueASCII < 65 && valueASCII!=32) || (valueASCII > 90 && valueASCII != 165)){
-                    result = false;
-                }
-            }
-            index++;
-        }
-        return result;
-    }
 }
