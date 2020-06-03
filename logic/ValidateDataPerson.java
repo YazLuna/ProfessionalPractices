8edbe7ca74dd67d6c3f2f85ef50c7bd0a9d4719b
Martha-Miroslava <61640956+Martha-Miroslava@users.noreply.github.com>
2020-06-03 01:36:45 -0500

    Update ValidateDataPerson.java

--- logic/ValidateDataPerson.java
+++ logic/ValidateDataPerson.java
@@ -8,49 +8,19 @@ import java.util.regex.Matcher;
  * @author MARTHA
  * @version 08/05/2020
  */
-public class ValidateDataPerson {
-
-    /**
-     * Method to validate name
-     * @param name The name parameter defines the name of the person
-     * @return If the name is valid
-     */
-    public boolean validateName (String name) {
-        boolean result;
-        Pattern pattern = Pattern
-                .compile("[A-Za-z]{3,50}");
-        Matcher mather = pattern.matcher(name);
-        result = mather.find();
-        return result;
-    }
-
+public class ValidateDataPerson extends ValidateGeneral {
     /**
      * Method to validate last name
      * @param lastName  The last name parameter defines the last name of the person
      * @return If the last name is valid
      */
     public boolean validateLastName (String lastName) {
-        boolean result;
+        boolean isValidLastName;
         Pattern pattern = Pattern
                 .compile("[A-Za-z]{3,50}");
         Matcher mather = pattern.matcher(lastName);
-        result = mather.find();
-        return result;
-    }
-
-    /**
-     * Method to validate email
-     * @param email The email parameter defines the email
-     * @return If the email is valid
-     */
-    public boolean validateEmail (String email) {
-        boolean result;
-        Pattern pattern = Pattern
-                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        Matcher mather = pattern.matcher(email);
-        result = mather.find();
-        return result;
+        isValidLastName = mather.find();
+        return isValidLastName;
     }
 
     /**
@@ -59,11 +29,11 @@ public class ValidateDataPerson {
      * @return If the charge is valid
      */
     public boolean validateCharge (String charge) {
-        boolean result;
+        boolean isValidCharge;
         Pattern pattern = Pattern
                 .compile("[A-Za-z]{6,25}");
         Matcher mather = pattern.matcher(charge);
-        result = mather.find();
-        return result;
+        isValidCharge = mather.find();
+        return isValidCharge;
     }
 }
